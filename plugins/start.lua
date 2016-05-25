6ec1e1d9e8fb24640028924561e0e2c96afd6a69
Armin <armin.bagheri.1380@gmail.com>
2016-05-25 17:48:16 +0430

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -1,6 +1,6 @@
 local action function(msg, matches)
   local text = " *سلام*\n\n`به ربات پیامرسان آرمین خوش آمدید`\n\n _لطفا پیام خود را مانند نمونه زیر ارسال کنید_\n\n/c [Text]"
-  api.sendReply(msg,text,true
+  api.sendReply(msg,text,true)
   end
 local triggers = {
   "^/[Ss][Tt][Aa][Rr][Tt]",
