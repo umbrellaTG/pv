dd3269b975cd965fc7ecf27321a79a0e351937c2
Armin <armin.bagheri.1380@gmail.com>
2016-05-25 17:16:22 +0430

    Update private.lua

--- plugins/private.lua
+++ plugins/private.lua
@@ -3,7 +3,7 @@ local function do_keybaord_credits()
     keyboard.inline_keyboard = {
     	{
     		{text = 'Channel', url = 'https://telegram.me/'..config.channel:gsub('@', '')},
-    		{text = 'GitHub', url = 'https://github.com/RememberTheAir/GroupButler'},
+    		{text = 'GitHub', url = 'https://github.com'},
     		{text = 'Rate me!', url = 'https://telegram.me/storebot?start='..bot.username},
 		}
 	}
